b3db6df0b0a10f768ef1cea420af2523ace89341
Akram Ben Aissi <akram.benaissi@gmail.com>
2023-04-01 08:36:54 +0200

    Extraction DistributionFunction to allow passing different type of functions for it
    Signed-off-by: Akram Ben Aissi <akram.benaissi@gmail.com>

--- controller/sharding/sharding.go
+++ controller/sharding/sharding.go
@@ -1,13 +1,25 @@
 package sharding
 
 import (
+	"context"
 	"fmt"
 	"hash/fnv"
+	"math"
 	"os"
+	"reflect"
+	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 
+	"github.com/argoproj/argo-cd/v2/common"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+
+	"github.com/argoproj/argo-cd/v2/util/db"
+	"github.com/argoproj/argo-cd/v2/util/env"
+	"github.com/argoproj/argo-cd/v2/util/settings"
+	log "github.com/sirupsen/logrus"
+	"k8s.io/client-go/kubernetes"
 )
 
 func InferShard() (int, error) {
@@ -23,31 +35,90 @@ func InferShard() (int, error) {
 	if err != nil {
 		return 0, fmt.Errorf("hostname should ends with shard number separated by '-' but got: %s", hostname)
 	}
-	return shard, nil
+	return int(shard), nil
 }
 
-// GetShardByID calculates cluster shard as `clusterSecret.UID % replicas count`
-func GetShardByID(id string, replicas int) int {
-	if id == "" {
-		return 0
-	} else {
-		h := fnv.New32a()
-		_, _ = h.Write([]byte(id))
-		return int(h.Sum32() % uint32(replicas))
-	}
-}
-
-func GetClusterFilter(replicas int, shard int) func(c *v1alpha1.Cluster) bool {
+func GetClusterFilter(distributionFunction DistributionFunction, shard int) ClusterFilterFunction {
 	return func(c *v1alpha1.Cluster) bool {
-		clusterShard := 0
-		//  cluster might be nil if app is using invalid cluster URL, assume shard 0 in this case.
-		if c != nil {
-			if c.Shard != nil {
-				clusterShard = int(*c.Shard)
-			} else {
-				clusterShard = GetShardByID(c.ID, replicas)
-			}
-		}
-		return clusterShard == shard
+		return distributionFunction(c) == shard
 	}
 }
+
+func GetDistributionFunction(kubernetesClient kubernetes.Interface, settingsMgr *settings.SettingsManager) DistributionFunction {
+	filterFunctionName := env.StringFromEnv(common.EnvControllerShardingAlgorithm, "legacy")
+	distributionFunction := GetShardByIdUsingHashDistributionFunction()
+	log.Infof("Using filter function:  %s", filterFunctionName)
+	switch {
+	case filterFunctionName == "hash":
+		distributionFunction = GetShardByIndexModuloReplicasCountDistributionFunction(settingsMgr, kubernetesClient)
+	case filterFunctionName == "legacy":
+		distributionFunction = GetShardByIdUsingHashDistributionFunction()
+	default:
+		distributionFunctionName := runtime.FuncForPC(reflect.ValueOf(distributionFunction).Pointer())
+		log.Warnf("No distribution function named '%s' found. Defaulting to '%s'", filterFunctionName, distributionFunctionName)
+	}
+	return distributionFunction
+}
+
+func GetShardByIndexModuloReplicasCountDistributionFunction(settingsMgr *settings.SettingsManager, kubeClientset kubernetes.Interface) DistributionFunction {
+	replicas := env.ParseNumFromEnv(common.EnvControllerReplicas, 0, 0, math.MaxInt32)
+	ctx := context.Background()
+	db := db.NewDB(settingsMgr.GetNamespace(), settingsMgr, kubeClientset)
+	clustersList, dbErr := db.ListClusters(ctx)
+	if dbErr != nil {
+		log.Warnf("Error while querying clusters list from database: %v", dbErr)
+	}
+	clusters := clustersList.Items
+	sort.Slice(clusters, func(i, j int) bool {
+		return clusters[i].ID < clusters[j].ID
+	})
+	log.Debugf("ClustersList has %d items", len(clusters))
+	clusterById := make(map[string]v1alpha1.Cluster)
+	clusterIndexdByClusterId := make(map[string]int)
+	for i, cluster := range clusters {
+		log.Debugf("Adding cluster with id=%s and name=%s to clusterIndexdByClusterId map", cluster.ID, cluster.Name)
+		clusterById[cluster.ID] = cluster
+		clusterIndexdByClusterId[cluster.ID] = i
+	}
+
+	return func(c *v1alpha1.Cluster) int {
+		if c != nil && replicas != 0 {
+			clusterIndex, ok := clusterIndexdByClusterId[c.ID]
+			if !ok {
+				log.Infof("Cluster with id=%s not found in clusterIndexdByClusterId map", c.ID)
+			}
+			shard := int(clusterIndex % replicas)
+			log.Infof("Cluster with id=%s will be processed by shard %d", c.ID, shard)
+			return shard
+		}
+		log.Warnf("The number of replicas is set to 0 or the passed cluster is null: replicas: %d, cluster: %v", replicas, c)
+		return -1
+	}
+}
+
+func GetShardByIdUsingHashDistributionFunction() DistributionFunction {
+	replicas := env.ParseNumFromEnv(common.EnvControllerReplicas, 0, 0, math.MaxInt32)
+	return func(c *v1alpha1.Cluster) int {
+		if replicas == 0 {
+			return -1
+		}
+		if c == nil {
+			log.Infof("Calculating cluster shard for cluster id: %v", c)
+			return 0
+		}
+		id := c.ID
+		log.Infof("Calculating cluster shard for cluster id: %s", id)
+		if id == "" {
+			return 0
+		} else {
+			h := fnv.New32a()
+			_, _ = h.Write([]byte(id))
+			shard := int32(h.Sum32() % uint32(replicas))
+			log.Infof("Cluster with id=%s will be processed by shard %d", c.ID, shard)
+			return int(shard)
+		}
+	}
+}
+
+type DistributionFunction func(c *v1alpha1.Cluster) int
+type ClusterFilterFunction func(c *v1alpha1.Cluster) bool
--- controller/sharding/sharding_test.go
+++ controller/sharding/sharding_test.go
@@ -1,29 +1,163 @@
 package sharding
 
 import (
+	"os"
 	"testing"
 
+	"github.com/argoproj/argo-cd/v2/common"
 	"github.com/argoproj/argo-cd/v2/pkg/apis/application/v1alpha1"
+	"github.com/argoproj/argo-cd/v2/test"
 
+	"context"
+
+	"github.com/argoproj/argo-cd/v2/util/db"
+	"github.com/argoproj/argo-cd/v2/util/settings"
 	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/fake"
 )
 
+type fakeData struct {
+	apps          []runtime.Object
+	configMapData map[string]string
+}
+
 func TestGetShardByID_NotEmptyID(t *testing.T) {
-	assert.Equal(t, 0, GetShardByID("1", 2))
-	assert.Equal(t, 1, GetShardByID("2", 2))
-	assert.Equal(t, 0, GetShardByID("3", 2))
-	assert.Equal(t, 1, GetShardByID("4", 2))
+	os.Setenv(common.EnvControllerReplicas, "1")
+	assert.Equal(t, 0, GetShardByIdUsingHashDistributionFunction()(&v1alpha1.Cluster{ID: "1"}))
+	assert.Equal(t, 0, GetShardByIdUsingHashDistributionFunction()(&v1alpha1.Cluster{ID: "2"}))
+	assert.Equal(t, 0, GetShardByIdUsingHashDistributionFunction()(&v1alpha1.Cluster{ID: "3"}))
+	assert.Equal(t, 0, GetShardByIdUsingHashDistributionFunction()(&v1alpha1.Cluster{ID: "4"}))
 }
 
 func TestGetShardByID_EmptyID(t *testing.T) {
-	shard := GetShardByID("", 10)
+	os.Setenv(common.EnvControllerReplicas, "1")
+	distributionFunction := GetShardByIdUsingHashDistributionFunction
+	shard := distributionFunction()(&v1alpha1.Cluster{})
 	assert.Equal(t, 0, shard)
 }
 
+func TestGetShardByID_NoReplicas(t *testing.T) {
+	os.Setenv(common.EnvControllerReplicas, "0")
+	distributionFunction := GetShardByIdUsingHashDistributionFunction
+	shard := distributionFunction()(&v1alpha1.Cluster{})
+	assert.Equal(t, -1, shard)
+}
+
 func TestGetClusterFilter(t *testing.T) {
-	filter := GetClusterFilter(2, 1)
+	shardIndex := 1 // ensuring that a shard with index 1 will process all the clusters with an "even" id (2,4,6,...)
+	os.Setenv(common.EnvControllerReplicas, "2")
+	filter := GetClusterFilter(GetDistributionFunction(nil, nil), shardIndex)
 	assert.False(t, filter(&v1alpha1.Cluster{ID: "1"}))
 	assert.True(t, filter(&v1alpha1.Cluster{ID: "2"}))
 	assert.False(t, filter(&v1alpha1.Cluster{ID: "3"}))
 	assert.True(t, filter(&v1alpha1.Cluster{ID: "4"}))
+}
+
+func TestGetShardByIndexModuloReplicasCountDistributionFunction2(t *testing.T) {
+	os.Setenv(common.EnvControllerReplicas, "2")
+	ctx := context.Background()
+	settingsMgr, clientset := newFakeClient(ctx)
+	distributionFunction := GetShardByIndexModuloReplicasCountDistributionFunction(settingsMgr, clientset)
+	db := db.NewDB(settingsMgr.GetNamespace(), settingsMgr, clientset)
+
+	cluster1 := createAndSaveCluster("cluster1", "123", db, ctx, t)
+	cluster2 := createAndSaveCluster("cluster2", "456", db, ctx, t)
+	cluster3 := createAndSaveCluster("cluster3", "789", db, ctx, t)
+	cluster4 := createAndSaveCluster("cluster4", "abc", db, ctx, t)
+	cluster5 := createAndSaveCluster("cluster5", "def", db, ctx, t)
+
+	// Test with replicas set to 1
+	os.Setenv(common.EnvControllerReplicas, "1")
+	assert.Equal(t, 1, distributionFunction(&cluster1))
+	assert.Equal(t, 1, distributionFunction(&cluster2))
+	assert.Equal(t, 1, distributionFunction(&cluster3))
+	assert.Equal(t, 1, distributionFunction(&cluster4))
+	assert.Equal(t, 1, distributionFunction(&cluster5))
+
+	// Test with replicas set to 2
+	os.Setenv(common.EnvControllerReplicas, "2")
+	assert.Equal(t, 0, distributionFunction(&cluster1))
+	assert.Equal(t, 1, distributionFunction(&cluster2))
+	assert.Equal(t, 0, distributionFunction(&cluster3))
+	assert.Equal(t, 1, distributionFunction(&cluster4))
+	assert.Equal(t, 0, distributionFunction(&cluster5))
+
+	// Test with replicas set to 3
+	os.Setenv(common.EnvControllerReplicas, "3")
+	assert.Equal(t, 0, distributionFunction(&cluster1))
+	assert.Equal(t, 1, distributionFunction(&cluster2))
+	assert.Equal(t, 2, distributionFunction(&cluster3))
+	assert.Equal(t, 0, distributionFunction(&cluster4))
+	assert.Equal(t, 1, distributionFunction(&cluster5))
+}
+
+func TestGetShardByIndexModuloReplicasCountDistributionFunction(t *testing.T) {
+	os.Setenv(common.EnvControllerReplicas, "2")
+	ctx := context.Background()
+	settingsMgr, clientset := newFakeClient(ctx)
+	db := db.NewDB(settingsMgr.GetNamespace(), settingsMgr, clientset)
+
+	cluster1 := createAndSaveCluster("cluster1", "123", db, ctx, t)
+	cluster2 := createAndSaveCluster("cluster2", "456", db, ctx, t)
+	distributionFunction := GetShardByIndexModuloReplicasCountDistributionFunction(settingsMgr, clientset)
+
+	// Test that the function returns the correct shard for cluster1 and cluster2
+	expectedShardForCluster1 := 0
+	expectedShardForCluster2 := 1
+	shardForCluster1 := distributionFunction(&cluster1)
+	shardForCluster2 := distributionFunction(&cluster2)
+
+	if shardForCluster1 != expectedShardForCluster1 {
+		t.Errorf("Expected shard for cluster1 to be %d but got %d", expectedShardForCluster1, shardForCluster1)
+	}
+	if shardForCluster2 != expectedShardForCluster2 {
+		t.Errorf("Expected shard for cluster2 to be %d but got %d", expectedShardForCluster2, shardForCluster2)
+	}
+}
+
+func createAndSaveCluster(name string, id string, db db.ArgoDB, ctx context.Context, t *testing.T) v1alpha1.Cluster {
+	cluster1 := v1alpha1.Cluster{
+		Name:   name,
+		ID:     id,
+		Server: "https://kubernetes.default.svc?" + id,
+	}
+
+	_, err := db.CreateCluster(ctx, &cluster1)
+	if err != nil {
+		t.Fatalf("Error creating cluster 1: %v", err)
+	}
+	return cluster1
+}
+
+func newFakeClient(ctx context.Context) (*settings.SettingsManager, kubernetes.Interface) {
+	data := &fakeData{apps: []runtime.Object{}}
+
+	secret := corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "argocd-secret",
+			Namespace: test.FakeArgoCDNamespace,
+		},
+		Data: map[string][]byte{
+			"admin.password":   []byte("test"),
+			"server.secretkey": []byte("test"),
+		},
+	}
+	cm := corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "argocd-cm",
+			Namespace: test.FakeArgoCDNamespace,
+			Labels: map[string]string{
+				"app.kubernetes.io/part-of": "argocd",
+			},
+		},
+		Data: data.configMapData,
+	}
+	var clust corev1.Secret
+	kubeClientset := fake.NewSimpleClientset(&clust, &cm, &secret)
+	settingsMgr := settings.NewSettingsManager(ctx, kubeClientset, test.FakeArgoCDNamespace)
+	return settingsMgr, kubeClientset
 }
